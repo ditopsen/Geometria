62a505f2e64050d5c7e71f625922243e1e21ca2d
ditopsen <jsgarciam3456@gmail.com>
2026-03-09 07:28:43 -0500

    Update geometria.py

--- geometria.py
+++ geometria.py
@@ -1,19 +1,33 @@
+def hacer_circulo():
+    print("haciendo un circulo")
+def hacer_cuadrado():
+    print("haciendo un cuadrado")
+def hacer_triangulo():
+    print("haciendo triangulo")
+def hacer_rombo():
+    print("haciendo rombo")
+def menu():
+    print("Menu de figuras geometricas")
 while True:
+    menu()
     try:
         print( "Bienvenido ")
-        opcion = (int(input(" seleccione la figura que desee hacer \n 1. un circulo \n 2. un cuadrado \n 3. un triangulo \n  4. un rombo \n 5. salir \n ")))
+        opcion = int(input(" seleccione la figura que desee hacer \n 1. un circulo \n 2. un cuadrado \n 3. un triangulo \n  4. un rombo \n 5. salir \n "))
         match opcion: 
             case 1:
-                print("ha elejido el circulo")
+                hacer_circulo()
             case 2:
-                print("ha elejido un cuadrado")
+                hacer_cuadrado()
             case 3: 
-                print(" ha elejido un triangulo")
+                hacer_triangulo()
             case 4:
-                print("has elejido un rombo")
+                hacer_rombo()
             case 5:
                 print("Haz elejido salir ")
                 break
+            case _: 
+                print("esta opcion no esta en el menu, porfavor elegir algo del menu")
+                continue 
     except ValueError:
         print(" No se pueden usar letras porfavor digite el numero que esta en pantallaza ")
-        print("HOLA")
+        
