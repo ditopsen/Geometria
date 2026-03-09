05903246586b073c2c22ccd6f227457a6612dde1
ditopsen <jsgarciam3456@gmail.com>
2026-03-09 09:51:27 -0500

    Update geometria.py

--- geometria.py
+++ geometria.py
@@ -1,10 +1,65 @@
+import matplotlib.pyplot as plt
+from matplotlib. patches import Circle
 def hacer_circulo():
     print("haciendo un circulo")
+    fig, ax = plt.subplots()
+
+    circulo = Circle((0, 0), 1, fill=False)  # (centro_x, centro_y), radio
+    ax.add_patch(circulo)
+
+    ax.set_aspect('equal')
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+
+    plt.title("Círculo")
+    plt.show()
 def hacer_cuadrado():
     print("haciendo un cuadrado")
+    fig, ax = plt.subplots()
+
+    # coordenadas del cuadrado
+    x = [-1, 1, 1, -1, -1]
+    y = [-1, -1, 1, 1, -1]
+
+    ax.plot(x, y)
+
+    ax.set_aspect('equal')
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+
+    plt.title("Cuadrado")
+    plt.show()
 def hacer_triangulo():
+    fig, ax = plt.subplots()
+
+    # coordenadas del triangulo
+    x = [0, -1, 1, 0]
+    y = [1, -1, -1, 1]
+
+    ax.plot(x, y)
+
+    ax.set_aspect('equal')
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+
+    plt.title("Triangulo")
+    plt.show()
     print("haciendo triangulo")
 def hacer_rombo():
+    fig, ax = plt.subplots()
+
+    # coordenadas del rombo
+    x = [0, 1, 0, -1, 0]
+    y = [1, 0, -1, 0, 1]
+
+    ax.plot(x, y)
+
+    ax.set_aspect('equal')
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+
+    plt.title("Rombo")
+    plt.show()
     print("haciendo rombo")
 def menu():
     print("Menu de figuras geometricas")
